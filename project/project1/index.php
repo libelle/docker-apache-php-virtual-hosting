4e3ade4c30672eed3c1c42087aa58913cdfb0da7
SjG <sjg@cmsmodules.com>
2021-07-09 13:49:56 -0700

    Oops. Cleanup.

--- project/project1/index.php
+++ project/project1/index.php
@@ -1,2 +1,11 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>This is Project 1</title>
+</head>
+<body>
+<h1>Welcome to Project 1</h1>
+<?php phpinfo(); ?>
+</body>
+</html>
